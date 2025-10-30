52b96d2b07f48e5082313a968fea27e8c0270265
BrenoOliveiradev <brenooliveira.dev@hotmail.com>
2025-10-29 23:38:45 -0300

    acrescentando useRef nos inputs

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,20 @@
+import { useRef } from "react"
+
+
 import { Title, Container, TopBackground, Form, ContainerInputs, Input, Button, InputLabel } from "./styles"
 
 import UsersImage from '../../assets/users.png'
 
 
 function Home() {
+  const inputName = useRef()
+  const inputAge = useRef()
+  const inputEmail = useRef()
+
+  function registerNewUser(){
+    console.log(inputName.current.value)
+
+  }
 
   return (
     <>
@@ -20,23 +31,23 @@ function Home() {
 
             <div>
               <InputLabel>Nome<span>*</span></InputLabel>
-              <Input type='text' placeholder='Nome do Usuário' />
+              <Input type='text' placeholder='Nome do Usuário' ref={inputName}/>
             </div>
 
             <div>
               <InputLabel>Idade<span>*</span></InputLabel>
-              <Input type='number' placeholder='Idade do Usuário' />
+              <Input type='number' placeholder='Idade do Usuário' ref={inputAge}/>
             </div>
 
           </ContainerInputs>
 
           <div style={{ width: '100%' }}>
             <InputLabel>E-mail<span>*</span></InputLabel>
-            <Input type='email' placeholder='Email do Usuário' />
+            <Input type='email' placeholder='Email do Usuário' ref={inputEmail}/>
           </div>
 
 
-          <Button>Cadastrar Usuário</Button>
+          <Button type="button" onClick={registerNewUser}>Cadastrar Usuário</Button>
 
         </Form>
 
